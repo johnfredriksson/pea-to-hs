c591e4e44bd1afc6ae1f9c2e496e0a6c3304d5fd
John Fredriksson <93582861+johnfredriksson@users.noreply.github.com>
2023-04-15 22:30:15 +0200

    added tests for createCompanyObject

---
+++ __tests__/units/move/createCompanyObject.test.js
@@ -0,0 +1,68 @@
+const createCompanyObject = require('../../../src/models/move')
+    .createCompanyObject;
+
+test('should return a company object with all properties', () => {
+  const client = {
+    name: 'clientName',
+    email: 'client@email.test',
+    phone: '123-4 12 34 56',
+    address: {
+      'address1': 'first address',
+      'address2': 'second address',
+      'zip-code': 'zip code',
+      'state': 'state',
+      'country': 'country',
+    },
+  };
+
+  const result = createCompanyObject(client);
+  expect(result).toEqual({
+    properties: {
+      name: 'clientName',
+      owneremail: 'client@email.test',
+      phone: '123-4 12 34 56',
+      domain: 'email.test',
+      address: 'first address',
+      address2: 'second address',
+      zip: 'zip code',
+      state: 'state',
+      country: 'country',
+    },
+  });
+});
+
+test('should return a company object with some properties', () => {
+  const client = {
+    name: 'clientName',
+    phone: '123-4 12 34 56',
+    address: {
+      address1: 'first address',
+      state: 'state',
+      country: 'country',
+    },
+  };
+
+  const result = createCompanyObject(client);
+  expect(result).toEqual({
+    properties: {
+      name: 'clientName',
+      phone: '123-4 12 34 56',
+      address: 'first address',
+      state: 'state',
+      country: 'country',
+    },
+  });
+});
+
+test('should return a company object with name property', () => {
+  const client = {
+    name: 'clientName',
+  };
+
+  const result = createCompanyObject(client);
+  expect(result).toEqual({
+    properties: {
+      name: 'clientName',
+    },
+  });
+});
